fd081770a9536360e86d0bb9bb000435fd6c2697
Justin Mach <justin.mach@mail.utoronto.ca>
2022-03-27 16:35:27 -0400

    Remove some comments

--- server/config.js
+++ server/config.js
@@ -3,7 +3,3 @@ const SECRET = "mySecretSHHHLOL"
 
 export {CONNECTION_URL, SECRET}
 
-
-// module.exports = {
-// CONNECTION_URL: "mongodb+srv://ajaxpictures:utscajax1109@ajaxpicturescluster.kyqfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-// SECRET: "mySecretSHHHLOL"}
