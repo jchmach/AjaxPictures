ea91c7086df42e6491e5df34059f917cd44c3574
Justin Mach <justin.mach@mail.utoronto.ca>
2022-03-28 00:45:29 -0400

    Added continuous deployment for server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,8 @@
-//const {ApolloServer} = require('apollo-server');
 import {ApolloServer} from 'apollo-server';
-// const mongoose = require('mongoose');
 import mongoose from 'mongoose'
 import typeDefs from './graphql/typeDefs.js';
 import resolvers from './graphql/resolvers/index.js';
 import { CONNECTION_URL } from './config.js';
-
-// const typeDefs = require('./graphql/typeDefs');
-// const resolvers = require('./graphql/resolvers')
-// const {CONNECTION_URL} = require('./config.js');
-
 
 const server = new ApolloServer ({
     typeDefs,
